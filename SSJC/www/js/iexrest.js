4759caa337e25a7c054eab59c040e77b5efb0f36
jfcd <jfdornheim@gmail.com>
2020-02-13 17:15:59 -0500

    cleanup

--- SSJC/www/js/iexrest.js
+++ SSJC/www/js/iexrest.js
@@ -3,6 +3,65 @@ const request = require('request');
 var stream;
 var partialMessage;
 
+var rowNum = 3;
+function addrow(){
+  var table = document.getElementById("stocktable");
+  var row = table.insertRow(-1);
+  rowNum = rowNum + 1;
+
+  var td1 = document.createElement("td");
+  var td2 = document.createElement("td");
+  var td3 = document.createElement("td");
+  var td4 = document.createElement("td");
+  var td5 = document.createElement("td");
+  row.appendChild(td1);
+  row.appendChild(td2);
+  row.appendChild(td3);
+  row.appendChild(td4);
+  row.appendChild(td5);
+
+  var cell1 = document.createElement("input");
+  cell1.setAttribute("style", "text-transform: uppercase");
+  cell1.setAttribute("type", "text");
+  var text = "stck" + rowNum.toString();
+  cell1.setAttribute("placeholder", text);
+  cell1.setAttribute("id", text);
+  td1.appendChild(cell1);
+  var cell2 = document.createElement("input");
+  cell2.setAttribute("type", "text");
+  cell2.setAttribute("placeholder", "# of Stocks");
+  //cell2.setAttribute("onchange")
+  td2.appendChild(cell2);
+  var cell3 = document.createElement("input");
+  cell3.setAttribute("disabled", "true");
+  cell3.setAttribute("type", "text");
+  cell3.setAttribute("placeholder", "Asking Price");
+  text = "ask" + rowNum.toString();
+  cell3.setAttribute("id", text);
+  td3.appendChild(cell3);
+  var cell4 = document.createElement("input");
+  cell4.setAttribute("disabled", "true");
+  cell4.setAttribute("type", "text");
+  cell4.setAttribute("placeholder", "Bid Price");
+  text = "bid" + rowNum.toString();
+  cell4.setAttribute("id", text)
+  td4.appendChild(cell4);
+  var cell5 = document.createElement("input");
+  cell5.setAttribute("type", "text");
+  cell5.setAttribute("disabled", "true");
+  cell5.setAttribute("placeholder", "Total Value");
+  //cell5.setAttribute("id", "value4")
+  td5.appendChild(cell5);
+}
+
+function deleterow() {
+  if(document.getElementById("stocktable").rows.length > 2){
+    document.getElementById("stocktable").deleteRow(-1);
+    rowNum = rowNum - 1;
+  }
+
+}
+
 function iexconnect(){
 
   if(stream){
