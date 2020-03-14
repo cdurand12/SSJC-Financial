8f346b8184f0a0d386b3d1377dd05cf2a1ec7fd7
cdurand12 <34111933+cdurand12@users.noreply.github.com>
2020-03-14 18:19:28 -0400

    rtest2 features R function being called. Still need to add Markowitz

--- SSJC/www/js/markowitz.js
+++ SSJC/www/js/markowitz.js
@@ -1,4 +1,4 @@
-ocpu.seturl("http://public.opencpu.org/ocpu/library/stats/R")
+ocpu.seturl("http://public.opencpu.org/ocpu/library/base/R")
 
 var mydata = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 
@@ -16,4 +16,31 @@ function rtest(){
     req.fail(function(){
         alert("R returned an error: " + req.responseText);
     });
+}
+
+
+function rtest2(){
+  console.log("rtest2 working");
+  var markfunc = "function(x, n){ return(x^n)}";
+
+  var mysnippet = new ocpu.Snippet(markfunc);
+
+  var req = ocpu.rpc("do.call", {
+       what : mysnippet,
+       args : {
+           x : [1,2,3,4,5],
+           n : 3
+       }
+   }, function(output){
+     console.log(output);
+   });
+
+   //if R returns an error, alert the error message
+   req.fail(function(){
+       alert("Server error: " + req.responseText);
+   });
+
+   req.always(function(){
+       $("button").removeAttr("disabled");
+});
 }
